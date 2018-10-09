3ca836e9f671b5a243af89954c4b5c2285bb8ea1
mohdtalibtothenew <mohd.talib@tothenew>
2018-10-09 13:27:39 +0530

    Fradle Exercise

---
+++ src/main/java1/Application1.java
@@ -0,0 +1,6 @@
+public class Application1 {
+    public static void main(String[] args) {
+        System.out.println("Application 1");
+        Application2.show();
+    }
+}
---
+++ src/main/java2/Application2.java
@@ -0,0 +1,5 @@
+public class Application2 {
+    public static void show(){
+        System.out.println("Application 2");
+    }
+}
